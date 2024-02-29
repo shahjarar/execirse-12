8cc0981940f8e3cddb47a411bb0734fcf23a7361
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:36:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var friendsName = ["Hamza", "Usman", "Ayesha", "Areeba"];
+friendsName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ",How are you")); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let friendsName = ["Hamza","Usman","Ayesha","Areeba"];
+
+friendsName.forEach(friendName => console.log(`Hello ${friendName},How are you`));
